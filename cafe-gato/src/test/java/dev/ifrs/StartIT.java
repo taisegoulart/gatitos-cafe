275310eaa30559f0bc80b486eef75abc2d11e204
Taíse Goulart <taise.m.goulart@gmail.com>
2023-06-14 08:50:24 -0300

    Add files via upload

---
+++ cafe-gato/src/test/java/dev/ifrs/StartIT.java
@@ -0,0 +1,8 @@
+package dev.ifrs;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class StartIT extends StartTest {
+    // Execute the same tests but in packaged mode.
+}
