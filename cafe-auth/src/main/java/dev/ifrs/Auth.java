ee5753b8e3989b953e1226bbc80f6d122ce79127
taisegoulart <taise.m.goulart@gmail.com>
2023-06-21 10:21:11 -0300

    Atualizando RestClient

---
+++ cafe-auth/src/main/java/dev/ifrs/Auth.java
@@ -0,0 +1,31 @@
+package dev.ifrs;
+
+import java.util.Arrays;
+import java.util.HashSet;
+
+import org.eclipse.microprofile.jwt.Claims;
+
+import io.smallrye.jwt.build.Jwt;
+import jakarta.annotation.security.PermitAll;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+@Path("/auth")
+public class Auth {
+
+    @GET
+    @Path("/jwt")
+    @PermitAll
+    @Produces(MediaType.TEXT_PLAIN)
+    public String generate() {
+        return Jwt.issuer("http://localhost:8080")
+            .upn("rodrigo@rpmhub.dev")
+            .groups(new HashSet<>(Arrays.asList("User", "Admin")))
+            .claim(Claims.full_name, "Rodrigo Prestes Machado")
+            .sign();
+}
+
+
+}
