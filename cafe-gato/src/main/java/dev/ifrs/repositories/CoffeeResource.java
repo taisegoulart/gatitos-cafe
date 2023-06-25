5a187d77375fb306938adc57e59d29e3f56a6f68
taisegoulart <taise.m.goulart@gmail.com>
2023-06-25 19:14:30 -0300

    Deletando teste de componente desnecessário

--- cafe-gato/src/main/java/dev/ifrs/repositories/CoffeeResource.java
+++ cafe-gato/src/main/java/dev/ifrs/repositories/CoffeeResource.java
@@ -3,15 +3,13 @@ package dev.ifrs.repositories;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.transaction.Transactional;
 import jakarta.ws.rs.Consumes;
-import jakarta.ws.rs.DELETE;
+
 import jakarta.ws.rs.GET;
-import jakarta.ws.rs.POST;
-import jakarta.ws.rs.PUT;
+
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
-import jakarta.ws.rs.core.MediaType;
-import jakarta.annotation.security.RolesAllowed;
+
 import java.util.List;
 import dev.ifrs.model.*;
 
